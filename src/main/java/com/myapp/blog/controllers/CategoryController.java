017e363e3932cdd7fd3655ea7d00e6fd2784e585
Raj Patel <patelraj24365@gmail.com>
2025-03-02 09:30:35 -0500

    implemented global error handling and delete category endpoint

--- src/main/java/com/myapp/blog/controllers/CategoryController.java
+++ src/main/java/com/myapp/blog/controllers/CategoryController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/api/v1/categories")
@@ -41,5 +42,12 @@ public class CategoryController {
                 categoryMapper.toDto(savedCategory),
                 HttpStatus.CREATED
         );
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable UUID id){
+        categoryService.deleteCategory(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
     }
 }
--- src/main/java/com/myapp/blog/services/CategoryService.java
+++ src/main/java/com/myapp/blog/services/CategoryService.java
@@ -3,10 +3,13 @@ package com.myapp.blog.services;
 import com.myapp.blog.domain.entities.Category;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CategoryService {
 
     List<Category> listCategories();
 
     Category createCategory(Category category);
+
+    void deleteCategory(UUID id);
 }
